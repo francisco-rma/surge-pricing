26aea573029308778e24c34b1b343265d8b5d04a
Denise Avila <denisefavila@gmail.com>
2024-12-18 17:39:44 -0300

    feat: isolate services

---
+++ start_producer.sh
@@ -0,0 +1,3 @@
+# Start the driver position producer (this will block until the producer stops)
+echo "Starting driver position producer..."
+python app/driver_position/driver_position_producer.py
--- start_services.sh
+++ start_services.sh
@@ -7,17 +7,6 @@ uvicorn app.main:app --host 0.0.0.0 --reload --reload-dir app &
 UVICORN_PID=$!
 echo "Started FastAPI server with PID $UVICORN_PID"
 
-# Start the driver position producer (this will block until the producer stops)
-echo "Starting driver position producer..."
-python app/driver_position/driver_position_producer.py &
-
-echo "Waiting for the driver position producer to initialize..."
-sleep 5  # Adjust this sleep time based on how long it takes the producer to start
-
-# Start the aggregator consumer (this will block until the consumer stops)
-echo "Starting the driver position aggregator..."
-python app/driver_position/aggregator_consumer.py &
-
 # Wait for the processes to finish
 wait $UVICORN_PID
 
